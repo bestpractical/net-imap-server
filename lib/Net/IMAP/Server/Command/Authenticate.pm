5cf0aabcdf2abb6578eff78d08a0fbf4399d8305
Alex Vandiver <alexmv@bestpractical.com>
2007-11-21 22:19:05 +0000

    * Some auths send the first line of data with the AUTHENTICATE    command (?)

--- lib/Net/IMAP/Server/Command/Authenticate.pm
+++ lib/Net/IMAP/Server/Command/Authenticate.pm
@@ -16,10 +16,10 @@ sub validate {
 
     my @options = $self->parsed_options;
     return $self->bad_command("Not enough options") if @options < 1;
-    return $self->bad_command("Too many options") if @options > 1;
+    return $self->bad_command("Too many options") if @options > 2;
 
     return $self->no_command("Login is disabled")
-      unless $self->connection->capability =~ /\bAUTH=@options\b/i;
+      unless $self->connection->capability =~ /\bAUTH=$options[0]\b/i;
 
     return 1;
 }
@@ -27,7 +27,7 @@ sub validate {
 sub run {
     my $self = shift;
 
-    my($type) = $self->parsed_options;
+    my($type, $arg) = $self->parsed_options;
     $self->server->auth_class->require || warn $@;
     my $auth = $self->server->auth_class->new;
     if ( $auth->provides_sasl( uc $type ) ) {
@@ -35,7 +35,7 @@ sub run {
         $self->sasl( $auth->$type() );
         $self->pending_auth($auth);
         $self->connection->pending(sub {$self->continue(@_)});
-        $self->continue("");
+        $self->continue( $arg || "");
     } else {
         $self->bad_command("Invalid login");
     }
