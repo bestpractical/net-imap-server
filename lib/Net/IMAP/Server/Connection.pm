dc9195e599841b46209065c1be5bb791170c0f76
Alex Vandiver <alexmv@bestpractical.com>
2010-06-07 14:53:54 -0400

    Ensure that EXAMINE followed by SELECT on the same mailbox correctly sets read-only-ness

--- lib/Net/IMAP/Server/Connection.pm
+++ lib/Net/IMAP/Server/Connection.pm
@@ -132,9 +132,10 @@ sub selected {
     # This is just being called as a getter
     return $self->_selected unless @_;
 
-    # This is a setter, but isn't actually changing the mailbox.
-    return $self->_selected if $mailbox and $self->_selected
-        and $mailbox eq $self->_selected;
+    # This is a setter, but isn't actually changing the mailbox, nor
+    # changing the read-only-ness.
+    return $self->_selected if ($mailbox || "") eq ($self->_selected || "")
+        and ($self->selected_read_only || 0) == ($read_only || 0);
 
     # Otherwise, flush any untagged messages, close the old, and open
     # the new.
